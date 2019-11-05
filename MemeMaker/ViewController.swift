6f8793e711c82858d555ff11ec0d41eb3ce1dd7e
Alikhan Batchaev <loko-han@mail.ru>
2019-11-05 15:06:24 +0300

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Алихан Батчаев on 05.11.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
