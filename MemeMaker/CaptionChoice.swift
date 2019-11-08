2fa3fc5443b0065150497169d5943e555ab310de
Alikhan Batchaev <loko-han@mail.ru>
2019-11-08 20:55:16 +0300

    Added arrays and logic to the segment control

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Алихан Батчаев on 05.11.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import Foundation
+
+struct CaptionOption {
+    let emoji: String
+    let caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var topCaptionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var bottomCaptionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var topCaptionLabel: UILabel!
+    @IBOutlet var bottomCaptionLabel: UILabel!
+    
+    var topChoices = [CaptionOption]()
+    var bottomChoices = [CaptionOption]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let coolChoice = CaptionOption(emoji: "🕶", caption: "You know what's cool?")
+        let madChoice = CaptionOption(emoji: "💥", caption: "You know what makes me mad?")
+        let loveChoice = CaptionOption(emoji: "💕", caption: "You know what o love?")
+        
+        topChoices = [coolChoice, madChoice, loveChoice]
+        
+        topCaptionSegmentedControl.removeAllSegments()
+        for choice in topChoices {
+            topCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+        topCaptionSegmentedControl.selectedSegmentIndex = 0
+        
+        let catChoice = CaptionOption(emoji: "🐱", caption: "Cats wearing hats")
+        let dogChoice = CaptionOption(emoji: "🐶", caption: "Dogs carrying logs")
+        let monkeyChoice = CaptionOption(emoji: "🐵", caption: "Monkeys being funky")
+        
+        bottomChoices = [catChoice, dogChoice, monkeyChoice]
+        
+        bottomCaptionSegmentedControl.removeAllSegments()
+        for choice in bottomChoices {
+            bottomCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
+        }
+        
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0
+        
     }
-
-
+    
+    
+    @IBAction func choiceSelected(_ sender: UISegmentedControl) {
+    }
+    
 }
 
